8344bf6399d2552a6fadd0acc888cb98ec1c3240
Natura <2216181639@qq.com>
2020-01-11 21:44:25 +0800

    LinkedList

---
+++ 链表/2-add-LinkedList/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        LinkedList<Integer> list = new LinkedList<>();
+
+        // 添加元素
+        list.addFirst(55);
+        list.add(1, 44);
+        list.addLast(66);
+
+
+        System.out.println(list);
+    }
+}
