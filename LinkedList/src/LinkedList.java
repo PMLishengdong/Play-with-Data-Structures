3d2fbbefddea6d6a0e6cc6d0cc7f251c2236167d
Natura <2216181639@qq.com>
2020-01-11 22:49:01 +0800

    updata

--- LinkedList/src/LinkedList.java
+++ LinkedList/src/LinkedList.java
@@ -1,25 +1,25 @@
 public class LinkedList<E> {
 
-    private class Node{
+    private class Node {
 
         E e;
         Node next;
 
-        public Node(E e, Node next){
+        public Node(E e, Node next) {
             this.e = e;
             this.next = next;
         }
 
-        public Node(E e){
+        public Node(E e) {
             this(e, null);
         }
 
-        public Node(){
+        public Node() {
             this(null, null);
         }
 
         @Override
-        public String toString(){
+        public String toString() {
             return e.toString();
         }
     }
@@ -27,6 +27,19 @@ public class LinkedList<E> {
     private Node head;
     private int size;
 
+    // 构造函数, 传入数组构造LinkedList
+    public LinkedList(E[] arr){
+
+        if(arr == null)
+            throw new IllegalArgumentException("Is Empty Array");
+
+        for(int i = arr.length - 1; i >= 0; i --){
+            head = new Node(arr[i], head);
+            size ++;
+        }
+    }
+
+    // 默认构造函数
     public LinkedList(){
         head = null;
         size = 0;
@@ -46,7 +59,7 @@ public class LinkedList<E> {
     public void add(int index, E e){
 
         if(index < 0 || index > size)
-            throw new IllegalArgumentException("添加失败， 索引非法的: " + index);
+            throw new IllegalArgumentException("index illegal");
 
         head = add(head, index, e);
     }
@@ -74,10 +87,112 @@ public class LinkedList<E> {
         add(size, e);
     }
 
+    // 返回在链表中index位置的元素
+    public E get(int index){
+        if(index < 0 || index >= size)
+            throw new IllegalArgumentException("index illega");
+        return get(head, index).e;
+    }
+
+    // 返回链表中第一个位置的元素
+    public E getFirst(){
+        return get(0);
+    }
+
+    // 返回链表中最后一个位置的元素
+    public E getLast(){
+        return get(size - 1);
+    }
+
+    // 返回以node为节点的链表中第index位置的节点, 递归算法
+    private Node get(Node node, int index){
+
+        if(index == 0)
+            return node;
+        else
+            return get(node.next, index - 1);
+    }
+
+    // 在链表中更新第index位置的元素
+    public void set(int index, E e){
+        if(index < 0 || index >= size)
+            throw new IllegalArgumentException("index is illega");
+//        Node node = get(head, index);
+//        node.e = e;
+        get(head, index).e = e;
+    }
+
+    // 在链表中更新第一个位置的元素
+    public void setFirst(E e){
+        set(0, e);
+    }
+
+    // 在链表中更新最后一个位置的元素
+    public void setLast(E e){
+        set(size - 1, e);
+    }
+
+    // 在链表中删除第index位置的元素, 返回删除元素的值
+    public E remove(int index){
+
+        if(index < 0 || index >= size)
+            throw new IllegalArgumentException("index is illega");
+
+        Node ret = get(head, index);
+        head = remove(head, index);
+
+        return ret.e;
+    }
+
+    // 在链表中删除第一个位置的元素, 返回删除元素的值
+    public E removeFirst(){
+        return remove(0);
+    }
+
+    // 在链表中删除最后一个位置的元素, 返回删除元素的值
+    public E removeLast(){
+        return remove(size - 1);
+    }
+
+    // 在以node为节点的链表中删除第index位置的元素
+    // 返回新的链表
+    private Node remove(Node node, int index){
+
+        if(index == 0){
+            size --;
+            return node.next;
+        }
+
+        node.next = remove(node.next, index - 1);
+        return node;
+    }
+
+    // 在链表中删除所有元素为e的节点
+    public void removeAll(E e){
+        head = removeAll(head, e);
+    }
+
+    // 在以node为节点的链表中删除所有元素为e的节点, 递归算法
+    // 返回新的链表
+    private Node removeAll(Node node, E e){
+
+        if(node == null)
+            return node;
+
+        node.next = removeAll(node.next, e);
+        if(node.e.equals(e)){
+            size ++;
+            return node.next;
+        }
+
+        return node;
+    }
+
     @Override
     public String toString(){
 
         StringBuilder res = new StringBuilder();
+        res.append(String.format("LinkedList: size = %d\n", size));
         for(Node cur = head; cur != null; cur = cur.next)
             res.append(cur.e + "->");
 
--- LinkedList/src/Main.java
+++ LinkedList/src/Main.java
@@ -3,14 +3,24 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
 
-        LinkedList<Integer> list = new LinkedList<>();
+        LinkedList<Integer> list = new LinkedList<>(new Integer[]{-11, 22, -33, 44, -55});
 
         // 添加元素
-        list.addFirst(55);
-        list.add(1, 44);
+        list.addFirst(01);
+        list.add(list.getSize() / 2, list.get(list.getSize() / 2) - 1);
         list.addLast(66);
+        System.out.println(list);
 
+        // 删除元素
+        list.removeFirst();
+        list.remove(list.getSize() / 2 - 1);
+        list.removeLast();
+        System.out.println(list);
 
+        // 修改 and 查询元素
+        list.setFirst(Math.abs(list.getFirst()));
+        list.set(list.getSize() / 2, Math.abs(list.get(list.getSize() / 2)));
+        list.setLast(Math.abs(list.getLast()));
         System.out.println(list);
     }
 }
